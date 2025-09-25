14745fc383df95c4c84d77869c66c7b5d1c1fa07
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-09-25 11:28:28 +0700

    add remarks on quickSort

--- quickSort.js
+++ quickSort.js
@@ -31,3 +31,13 @@ function quickSort(arr) {
 const arr = [64, 25, 42, 55, 12, 22, 11, 20, 70, 5, 7, 3, 2];
 console.log('Original:', arr);
 console.log('Sorted:', quickSort(arr));
+
+/*
+Worst case scenario: this happens when the most significant element is consistently chosen as a pivot point. This will cause a loop to iterate over every element n from the left. The split will cause a search of every element on the right with none on the left, O(n^2).
+
+Average case scenario: an average pivot point is selected at every call. This will reduce the number of additional iterations required. So, there will be n iterations and an ever-decreasing logn iterative calls, O(n*logn).
+
+Best case scenario: The middle value is always selected, and the iteration space is halved at every iteration, O(n*logn).
+
+The iterative nature of the algorithm will impact the space complexity because the function call and variables are retained on the stack while the calculations are performed. However, the decision to use an in-place swap means no new array needs to be created, O(log n).
+*/
