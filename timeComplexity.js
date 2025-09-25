c7d3159d4aead3a24bbc63e9b016513b5a5ed0f2
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-09-25 10:30:14 +0700

    remarks

--- timeComplexity.js
+++ timeComplexity.js
@@ -14,4 +14,15 @@ for (i = 0; i < n - 1; i++) {
 }
 
 console.log(arr);
-// O(n^2)
+
+// Time: O(n^2), Space: O(1)
+
+/*
+Worst case scenario: Given a list sorted in reverse order, how many comparisons are made? The inner and outer loop will have to run n times so it can be determined that worst case = O(n^2).
+
+Average comparison: Regardless of the order of the list, every item must be checked against average case = O(n^2).
+
+Best comparison: Given a sorted list, how many comparisons must be made? Again, regardless of the items in the list, every item must be checked, so best case = O(n^2).
+
+Finally, what is the space complexity of this approach? Because an in-place swap is being performed, no temporary array is required. There are three temporary variables i, j and min_index; however, these are not dependent on the list size. So, the image doubles the list, and the space complexity does not increase accordingly. Therefore, space complexity = O(1).
+*/
